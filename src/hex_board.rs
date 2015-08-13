87532739871a8c662d4ad381ba7f90b825f77c8e
Jason Schein <jschein@apple.com>
2015-08-12 17:31:41 -0700

    New rendering approach.

--- src/hex_board.rs
+++ src/hex_board.rs
@@ -4,8 +4,7 @@ use piston_window::*;
 
 // Draw an entire grid of hexagons
 // grid size is the radius of hexagons surrounding the origin.
-pub fn render_board(c: Context, g: &mut G2d, selected: Option<Coordinate>, grid_size: i32, spacing: Spacing) {
-	
+pub fn render_board(selected: Option<Coordinate>, grid_size: i32, spacing: Spacing, c: Context, g: &mut G2d) {
 	let origin = Coordinate::new(0, 0);
 	let mut board: Vec<Coordinate> = Vec::new();
 	board.push(origin);
@@ -17,10 +16,195 @@ pub fn render_board(c: Context, g: &mut G2d, selected: Option<Coordinate>, grid_
 		if selected.is_some() && coordinate.clone() == selected.unwrap() {
 			render_hex(coordinate, spacing, [0.75, 0.25, 0.5, 1.0], c, g);	
 		} else {
-			render_hex(coordinate, spacing, [0.25; 4], c, g);	
+			render_hex(coordinate, spacing, [0.25; 4], c, g);
 		}
-		
 	}
+}
+
+pub type Point =  [f64; 2];
+pub type Triangle = [Point; 3];
+pub type Hexagon = [Point; 6];
+
+pub struct HexBoard {
+	hexes: Vec<PixelHex>
+}
+
+impl HexBoard {
+	pub fn new_radius(radius: i32, spacing: Spacing) -> HexBoard {
+
+		let origin = Coordinate::new(0, 0);
+		let mut board: Vec<PixelHex> = Vec::new();
+		board.push(PixelHex::new(origin, spacing, None));
+		for i in 1..radius + 1 {
+			board.append(
+				&mut origin.ring(i, Spin::CCW(XY)).iter().map(|&x|
+					PixelHex::new(x, spacing, None)
+				).collect::<Vec<PixelHex>>()
+			);
+		}
+
+		HexBoard {
+			hexes: board
+		}
+	}
+
+	pub fn select(&mut self, coordinate: Coordinate<i32>) {
+		match self.hexes.iter_mut().find(|x| x.coordinate == coordinate) {
+			Some(pixel_hex) => {
+				pixel_hex.state = if pixel_hex.state == PixelHexState::Selected {
+					PixelHexState::Default
+				} else {
+					PixelHexState::Selected
+				};
+			},
+			None => {}
+		};
+	}
+
+	pub fn set_triangle(&mut self, coordinate: Coordinate<i32>, filled_triangle: Option<i32>) {
+		match self.hexes.iter_mut().find(|x| x.coordinate == coordinate) {
+			Some(pixel_hex) => {
+				pixel_hex.filled_triangle = filled_triangle;
+			},
+			None => {}
+		};
+	}
+
+	pub fn get_triangle(&self, coordinate: Coordinate<i32>) -> Option<i32> {
+		match self.hexes.iter().find(|x| x.coordinate == coordinate) {
+			Some(pixel_hex) => {
+				pixel_hex.filled_triangle
+			},
+			None => { None }
+		}
+	}
+}
+
+pub fn render_hex_board(board: &HexBoard, c: Context, g: &mut G2d) {
+	for pixel_hex in board.hexes.iter() {
+		render_pixel_hex(pixel_hex, c, g);
+	}
+}
+
+struct PixelHex {
+	pub vertices: Hexagon,
+	pub coordinate: Coordinate<i32>,
+	pub spacing: Spacing,
+	pub center: Point,
+	pub state: PixelHexState,
+	pub rotation: i32,
+	pub filled_triangle: Option<i32>
+}
+
+impl PixelHex {
+	pub fn new(coordinate: Coordinate, spacing: Spacing, filled_triangle: Option<i32>) -> PixelHex {
+		let (x, y) = coordinate.to_pixel_float(spacing);
+		let center = [x as f64, y as f64];
+		PixelHex {
+			vertices: hex_vertices(center, spacing),
+			coordinate: coordinate,
+			spacing: spacing,
+			center: center,
+			state: PixelHexState::Default,
+			rotation: 0,
+			filled_triangle: filled_triangle
+		}
+	}
+}
+
+fn render_pixel_hex(hex: &PixelHex, c: Context, g: &mut G2d) {
+	let color = match hex.state {
+		PixelHexState::Selected => { [0.75, 0.25, 0.5, 1.0] },
+		PixelHexState::Default => { [0.25; 4] }
+	};
+
+	polygon(
+		color,
+		&hex.vertices,
+		c.transform,
+		g
+	);
+
+	let x = hex.center[0];
+	let y = hex.center[1];
+	let vertex_iter = (&hex.vertices).iter().enumerate();
+	for (i, vertex) in vertex_iter {
+
+		// Assignment using destructuring is unstable for arrays?
+		let (x0, y0) = (vertex[0], vertex[1]);
+		let current = i % 6;
+		let next = (i + 1) % 6;
+		let (x1, y1) = (hex.vertices[next][0], hex.vertices[next][1]);
+
+		let line = Line::new([0.0, 0.0, 0.0, 0.5], 0.5)
+			.shape(line::Shape::Round);
+
+		Line::draw(
+			&line,
+			[x0, y0, x1, y1],
+			&c.draw_state,
+			c.transform,
+			g
+		);
+	}
+
+	match hex.filled_triangle {
+		Some(i) => {
+			render_triangle_hex(hex.center, hex.spacing, i, [0.4, 0.4, 0.8, 1.0], c, g);
+		},
+		None => {}
+	};
+}
+
+#[derive(Eq, PartialEq)]
+enum PixelHexState {
+	Selected,
+	Default
+}
+
+pub struct TriangleHex {
+	pub c: Coordinate<i32>,
+	pub filled_triangle: i32,
+	pub spacing: Spacing,
+}
+
+pub fn render_triangle_hex(center: Point, spacing: Spacing, i: i32, color: [f32; 4], c: Context, g: &mut G2d) {
+
+	let triangle =  hex_triangle(center, spacing, i);
+
+	polygon(
+		color,
+		&triangle,
+		c.transform,
+		g
+	);
+
+	let line = Line::new([0.0, 0.0, 0.0, 0.5], 0.5)
+		.shape(line::Shape::Round);
+
+	Line::draw(
+		&line,
+		[triangle[0][0], triangle[0][1], triangle[1][0], triangle[1][1]],
+		&c.draw_state,
+		c.transform,
+		g
+	);
+
+	Line::draw(
+		&line,
+		[triangle[1][0], triangle[1][1], triangle[2][0], triangle[2][1]],
+		&c.draw_state,
+		c.transform,
+		g
+	);
+
+	Line::draw(
+		&line,
+		[triangle[0][0], triangle[0][1], triangle[2][0], triangle[2][1]],
+		&c.draw_state,
+		c.transform,
+		g
+	);
 }
 
 // Draw a single hexagon and its border.
@@ -87,20 +271,34 @@ pub fn axial_pixel_to_hex(x: f32, y: f32, spacing: Spacing) -> Coordinate {
 }
 
 // Get all the vertices for a hexagon
-fn hex_vertices(center: [f64; 2], spacing: Spacing) -> [[f64; 2]; 6] {
-	let mut vertices: [[f64; 2]; 6] = [[0.0; 2]; 6];
+fn hex_vertices(center: Point, spacing: Spacing) -> Hexagon {
+	let mut vertices: [Point; 6] = [[0.0; 2]; 6];
 	for i in 0..6usize {
-		vertices[i] = hex_vertex(center, spacing, i as u32);
+		vertices[i] = hex_vertex(center, spacing, i as i32);
 	}
 
 	vertices
 }
 
+// Get the coordinates of a triangle in a hex
+fn hex_triangle(center: Point, spacing: Spacing, i: i32) -> Triangle{
+
+	let current = i % 6;
+	let other = (i + 1) % 6;
+
+	return [
+		center,
+		hex_vertex(center, spacing, other),
+		hex_vertex(center, spacing, current)
+	];
+}
+
 // Get one of the vertexes of a hexagon ( 0 <= i < 6)
-fn hex_vertex(center: [f64; 2], spacing: Spacing, i: u32) -> [f64; 2] {
+fn hex_vertex(center: Point, spacing: Spacing, i: i32) -> Point {
+	let current = i % 6;
 	match spacing {
 		Spacing::PointyTop(size) => {
-			let angle_deg: f64 = 60.0 * i as f64 + 30.0;
+			let angle_deg: f64 = 60.0 * current as f64 + 30.0;
 			let angle_rad: f64 = ::std::f64::consts::PI / 180.0 * angle_deg;
 			return [
 				center[0] + size as f64 * angle_rad.cos(),
@@ -108,7 +306,7 @@ fn hex_vertex(center: [f64; 2], spacing: Spacing, i: u32) -> [f64; 2] {
 			];
 		},
 		Spacing::FlatTop(size) => {
-			let angle_deg: f64 = 60.0 * i as f64;
+			let angle_deg: f64 = 60.0 * current as f64;
 			let angle_rad: f64 = ::std::f64::consts::PI / 180.0 * angle_deg;
 			return [
 				center[0] + size as f64 * angle_rad.cos(),
--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,15 @@ fn main() {
     // Coordinates of the selected hex (Last clicked hex).
     let mut selected = Some(hex2d::Coordinate::new(0,0));
 
+    let mut board = HexBoard::new_radius(grid_size, spacing);
+
     for e in window {
         match e.event {
             Some(Event::Render(render_args)) => {
                 e.draw_2d(|c, g| {
                     clear([1.0, 1.0, 1.0, 1.0], g);
-                    render_board(c.trans(200.0, 200.0), g, selected, grid_size, spacing);
+                    //render_board(selected, grid_size, spacing, c.trans(200.0, 200.0), g);
+                    render_hex_board(&board, c.trans(200.0, 200.0), g);
                 });
             },
             Some(Event::Input(Input::Move(Motion::MouseCursor(x, y)))) => {
@@ -50,9 +53,48 @@ fn main() {
                 } else {
                     selected = Some(coord);
                 }
+
+                board.select(coord);
                 println!("clicked: {:?}", coord);
                 println!("selected: {:?}", selected);
                 println!("coordinates: {:?}", mouse_coords);
+            },
+            Some(Event::Input(Input::Press(Button::Keyboard(key)))) => {
+                let coord = axial_pixel_to_hex(mouse_coords.0 as f32, mouse_coords.1 as f32, spacing);
+                let triangle_op = board.get_triangle(coord);
+                let mut triangle_id = match triangle_op {
+                    Some(i) => { i },
+                    None => { 0 }
+                };
+                match key {
+                    Key::Up => {
+                        if triangle_op.is_some() {
+                            board.set_triangle(coord, Some(triangle_id + 1));
+                        }
+                    },
+                    Key::Down => {
+                        if triangle_op.is_some() {
+                            board.set_triangle(coord, Some(triangle_id - 1));
+                        }
+                    },
+                    Key::Space => {
+                        if (triangle_op.is_some()) {
+                            board.set_triangle(coord, None);
+                        } else {
+                            board.set_triangle(coord, Some(3));
+                        }
+                        
+                    }
+                    _ => {}
+                };
+                let coord = axial_pixel_to_hex(mouse_coords.0 as f32, mouse_coords.1 as f32, spacing);
+                if coord.x.abs() > grid_size || coord.y.abs() > grid_size {
+                    selected = None;
+                } else {
+                    selected = Some(coord);
+                }
+
+                
             }
             _ => {}
         }
