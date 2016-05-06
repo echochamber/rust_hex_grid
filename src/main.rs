c9e98f95bec77dffde5ef30c8501badd56773904
Jason Schein <jasonschein@gmail.com>
2016-05-06 16:54:46 -0700

    Updated dependencies. Fixed to work with updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -21,29 +21,30 @@ fn main() {
     // Variables for the piston window
     let opengl = OpenGL::V3_2;
     let (width, height) = (800, 800);
-    let window: PistonWindow =
+    let mut window: PistonWindow =
         WindowSettings::new("piston: HexGrid", (width, height))
         .exit_on_esc(true)
         .opengl(opengl)
-        .into();
+        .build()
+        .unwrap();
 
     // Where the mouse was last moved to.
     let mut mouse_coords = (0i32, 0i32);
     // Coordinates of the selected hex (Last clicked hex).
     let mut selected = Some(hex2d::Coordinate::new(0,0));
 
-    for e in window {
-        match e.event {
-            Some(Event::Render(render_args)) => {
-                e.draw_2d(|c, g| {
+    while let Some(e) = window.next() {
+        match e {
+            Event::Render(render_args) => {
+                window.draw_2d(&e, |c, g| {
                     clear([1.0, 1.0, 1.0, 1.0], g);
                     render_board(c.trans(200.0, 200.0), g, selected, grid_size, spacing);
                 });
             },
-            Some(Event::Input(Input::Move(Motion::MouseCursor(x, y)))) => {
+            Event::Input(Input::Move(Motion::MouseCursor(x, y))) => {
                 mouse_coords = (x as i32 - 200, y as i32 - 200);
             },
-            Some(Event::Input(Input::Press(Button::Mouse(_)))) => {
+            Event::Input(Input::Press(Button::Mouse(_))) => {
                 let coord = axial_pixel_to_hex(mouse_coords.0 as f32, mouse_coords.1 as f32, spacing);
                 if coord.x.abs() > grid_size || coord.y.abs() > grid_size {
                     selected = None;
